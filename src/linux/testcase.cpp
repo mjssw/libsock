1e1ab75d259b39b522529ef560f7bdcef0aa5325
sirtoozee <sirtoozee@1d15e518-f7d5-11dd-a320-db7d8e7a638f>
2009-02-21 08:58:14 +0000

    Initial sync class object.

---
+++ src/linux/testcase.cpp
@@ -0,0 +1,25 @@
+// (C) Copyright 2008 - 2009 by CCTEC ENGINEERING CO., LTD
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+
+//-----------------------------------------------------------------------------
+//----- $Id$
+//-----------------------------------------------------------------------------
+
+#include "sync.h"
+
+int main() 
+{
+    CCTEC::Mutex pMutex;
+
+    pMutex.Lock();
+    pMutex.Unlock();
+    return 0;
+}
+
